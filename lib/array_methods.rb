59241279530569abcaf20988071dd64edfe87b7f
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-09-09 22:25:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-
+  
 end
 
 def using_reverse(array)
@@ -19,5 +19,5 @@ def using_last(array)
 end
 
 def using_size(array)
-  
+  array.size
 end
